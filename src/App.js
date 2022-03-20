851a6178538d6b1fcbf689a9867f5332a7dbe6a4
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-03-20 15:32:22 +0000

    Add styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { evaluate } from "mathjs"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const  App = () => {
 
-export default App;
+  const buttons = ["7", "8", "9", "c", "4", "5", "6", "*", "1", "2", "3", "/", "+", "0", "-", "="]
+  const [display, setDisplay] = useState("");
+  
+  const handler = (button) => {
+    if (button === "=") {
+      setDisplay(evaluate(display))
+    } else if (button === "c") {
+      setDisplay("")
+    }else{
+      setDisplay(display + button)
+    }
+     
+    }
+
+   return (
+     <div>
+       <h1>React Calculator</h1>
+       <h2>{display}</h2>
+       {buttons.map((button, index) => {
+        //  if (button.innerText === 0) {
+        //   let x = document.getElementsByTagName('button');
+        //   for (let i= buttons.length; i-->0;)
+        //       if (x[i].innerText==='=')
+        //           x[i].parentNode.style.backgroundColor= 'black'; 
+        //           console.log(x);         
+        //  }
+         return <button className="calc_key" key={index} onClick={() => handler(button)}>{button}</button>
+       })
+      }
+     </div>
+
+   )
+};
+
+
+export default App
