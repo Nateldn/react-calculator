7a39a639ba50a3869772e3b38ac3b904976fa18b
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-03-20 15:40:44 +0000

    Intitial commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
 import { useState } from "react";
-import { evaluate } from "mathjs"
+import { evaluate } from "mathjs";
 
-import './App.css';
+import "./App.css";
 
-const  App = () => {
-
-  const buttons = ["7", "8", "9", "c", "4", "5", "6", "*", "1", "2", "3", "/", "+", "0", "-", "="]
+const App = () => {
+  const buttons = [
+    "7",
+    "8",
+    "9",
+    "c",
+    "4",
+    "5",
+    "6",
+    "*",
+    "1",
+    "2",
+    "3",
+    "/",
+    "+",
+    "0",
+    "-",
+    "=",
+  ];
   const [display, setDisplay] = useState("");
-  
+
   const handler = (button) => {
     if (button === "=") {
-      setDisplay(evaluate(display))
+      setDisplay(evaluate(display));
     } else if (button === "c") {
-      setDisplay("")
-    }else{
-      setDisplay(display + button)
+      setDisplay("");
+    } else {
+      setDisplay(display + button);
     }
-     
-    }
+  };
 
-   return (
-     <div>
-       <h1>React Calculator</h1>
-       <h2>{display}</h2>
-       {buttons.map((button, index) => {
+  return (
+    <div>
+      <h1>React Calculator</h1>
+      <h2>{display}</h2>
+      {buttons.map((button, index) => {
         //  if (button.innerText === 0) {
         //   let x = document.getElementsByTagName('button');
         //   for (let i= buttons.length; i-->0;)
         //       if (x[i].innerText==='=')
-        //           x[i].parentNode.style.backgroundColor= 'black'; 
-        //           console.log(x);         
+        //           x[i].parentNode.style.backgroundColor= 'black';
+        //           console.log(x);
         //  }
-         return <button className="calc_key" key={index} onClick={() => handler(button)}>{button}</button>
-       })
-      }
-     </div>
-
-   )
+        return (
+          <button
+            className='calc_key'
+            key={index}
+            onClick={() => handler(button)}
+          >
+            {button}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
-
-export default App
+export default App;
